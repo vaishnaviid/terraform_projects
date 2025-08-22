4c1285c8ac5a67bcec75d5c6840f69b8945e99ab
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-08-22 16:22:21 +0530

    ami chnged

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"      
+  region = "ap-south-1"      
   
 }
 resource "aws_instance" "my_first_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-0861f4e788f5069dd" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
 
   tags = {
